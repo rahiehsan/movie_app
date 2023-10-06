1d2f536c93ac7e83512f1ac88f6b7fe101a93f05
rahiehsan <rahiehsan045@gmail.com>
2023-10-06 18:09:51 +0530

    first commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,23 +2,25 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/api/movieApi";
 import {APIKey} from "../../common/api/movieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
-
+  const movieText = "Harry";
+  const dispatch = useDispatch();
   useEffect(() => {
-    const movieText = "Harry";
     const fetchMovies = async () => {
       const response = await movieApi
       .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
       .catch((err) => {
         console.log("Err :", err);
       });
-      console.log("Response from API (Home.js) ",response);
+      dispatch(addMovies(response.data))
+      console.log("Response from API (Home.js) ",response.data);
     };
 
     fetchMovies()
-  }, []);
-
+  }, [dispatch]);
   return (
     <div>
       <div className="banner-img"></div>
